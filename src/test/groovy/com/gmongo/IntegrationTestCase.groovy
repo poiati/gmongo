24c933663e8a5d711ae8ee4c10dfa669b9fe5142
Stefan Kendall <stefankendall@gmail.com>
2012-02-02 14:32:33 -0500

    Removed empty tests by making base class abstract. Made mongo port configurable for tests

--- src/test/groovy/com/gmongo/IntegrationTestCase.groovy
+++ src/test/groovy/com/gmongo/IntegrationTestCase.groovy
@@ -3,17 +3,16 @@ package com.gmongo
 import com.mongodb.DBAddress
 import com.mongodb.WriteConcern
 
-class IntegrationTestCase extends GroovyTestCase {
+abstract class IntegrationTestCase extends GroovyTestCase {
 
   static DB_NAME = 'gmongo_test'
 
   def mongo, db
 
   void setUp() {
-    mongo = new GMongo(new DBAddress('localhost', 27017, DB_NAME))
+    int port = System.getProperty("MONGO_PORT", "27017") as int
+    mongo = new GMongo(new DBAddress('localhost', port, DB_NAME))
     mongo.setWriteConcern(WriteConcern.SAFE)
     db = mongo.getDB(DB_NAME)
   }
-
-  void testNothing() {}
 }
